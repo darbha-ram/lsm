b2ff703131c0e340ccae6367849ae7ccb05d41e9
Ram Darbha <darbha.ram@gmail.com>
2025-08-13 15:26:48 +0000

    Add test convert hash between bytes32 and string
    Hash is a 66-char string, 0x + 64 chars, where every 2 chars like "a8"
    is one byte. Test uses a fixed hash of 66 chars such as "0xa283...90",
    coverts it to bytes32, and back to string. Verifies that initial and
    final strings are the same.
    
    Also checks each byte of the bytes32 (after converting that bytes32
    to raw bytes using ethers.getBytes()) to ensure its numerical value
    matches what is expected from the fixed string.
    
    Test passes.

--- test/basic.js
+++ test/basic.js
@@ -47,17 +47,63 @@ describe("Libraries", function () {
         expect(res).to.equal("12Aug.1510");
 
     });
-
     
     it("create new payment leg", async function () {
         const fromAddr = "0xE74D3B7eC9Ad1E2341abc69D22F2820B88d4D62b";
         const toAddr   = "0xD8dfE02d0eD3Ff0E9fc100EdE06244c28d6f3655";
-        const res = await myLibsTest.callNewLeg(fromAddr, toAddr, 1000, toAddr);
+        const res = await myLibsTest.callNewLeg(fromAddr, toAddr, 100257, toAddr);
         expect(res.from).to.equal(fromAddr);
         expect(res.to).to.equal(toAddr);
-        expect(res.amount).to.equal(1000n);
+        expect(res.amount).to.equal(100257n);
         expect(res.erc20).to.equal(toAddr);
+    });
 
+    it("convert hash between bytes32 and string", async function () {
+        const idStringOrig = "0x4a520f0a421f62b0f867894d6c6351ac3ca1fad8ee6247601c7c0e5768d96ab2";
+        const idBytes      = await myLibsTest.callHexStringToBytes32(idStringOrig);
+        const idStringNew  = await myLibsTest.callToHexString(idBytes);
+
+        expect(idStringNew.length).to.equal(66); // 64 + '0x' prefix
+        expect(idStringNew).to.equal(idStringOrig);
+
+        const rawBytes = ethers.getBytes(idBytes);
+        expect(rawBytes.length).to.equal(32);
+        expect(rawBytes[0]).to.equal(0x4a);
+        expect(rawBytes[1]).to.equal(0x52);
+        expect(rawBytes[2]).to.equal(0x0f);
+        expect(rawBytes[3]).to.equal(0x0a);
+        expect(rawBytes[4]).to.equal(0x42);
+        expect(rawBytes[5]).to.equal(0x1f);
+        expect(rawBytes[6]).to.equal(0x62);
+        expect(rawBytes[7]).to.equal(0xb0);
+        expect(rawBytes[8]).to.equal(0xf8);
+        expect(rawBytes[9]).to.equal(0x67);
+
+        expect(rawBytes[10]).to.equal(0x89);
+        expect(rawBytes[11]).to.equal(0x4d);
+        expect(rawBytes[12]).to.equal(0x6c);
+        expect(rawBytes[13]).to.equal(0x63);
+        expect(rawBytes[14]).to.equal(0x51);
+        expect(rawBytes[15]).to.equal(0xac);
+        expect(rawBytes[16]).to.equal(0x3c);
+        expect(rawBytes[17]).to.equal(0xa1);
+        expect(rawBytes[18]).to.equal(0xfa);
+        expect(rawBytes[19]).to.equal(0xd8);
+
+        expect(rawBytes[20]).to.equal(0xee);
+        expect(rawBytes[21]).to.equal(0x62);
+        expect(rawBytes[22]).to.equal(0x47);
+        expect(rawBytes[23]).to.equal(0x60);
+        expect(rawBytes[24]).to.equal(0x1c);
+        expect(rawBytes[25]).to.equal(0x7c);
+        expect(rawBytes[26]).to.equal(0x0e);
+        expect(rawBytes[27]).to.equal(0x57);
+        expect(rawBytes[28]).to.equal(0x68);
+        expect(rawBytes[29]).to.equal(0xd9);
+        expect(rawBytes[30]).to.equal(0x6a);
+        expect(rawBytes[31]).to.equal(0xb2);
+
+        //console.log("hex string of ID is: ", idString);
     });
 
     
